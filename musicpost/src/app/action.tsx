e324b5730889642a9fde12930c8abc970e0a623c
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-10-06 04:44:09 +0900

    server actionsを追加

---
+++ musicpost/src/app/action.tsx
@@ -0,0 +1,59 @@
+"use server";
+import { RowDataPacket } from "mysql2";
+import { MappedTrack } from "@/types/mappedTrack";
+import { db } from "../lib/db";
+
+// mysql2ライブラリとの型の互換性を保証
+export interface Post extends RowDataPacket {
+    user_id: number;
+    music_id: string;
+    user_name: string;
+    content: string;
+}
+
+interface RawMusicData extends RowDataPacket {
+    id: string;
+    name: string;
+    album_art_url: string;
+    music_url: string;
+    artist: string;
+    artist_url: string;
+    preview_url: string | undefined;
+}
+
+export const getLatestPost = async () => {
+    try {
+        const [posts] = await db.query<Post[]>(`
+            select user_id, music_id, content ,user_name
+            from Posts 
+            order by id desc
+            limit 20
+        `);
+
+        const musicIds = posts.map((post) => post.music_id);
+
+        const [musicInfoRaw] = await db.query<RawMusicData[]>(
+            `
+            select id, name, album_art_url, music_url, artist, artist_url, preview_url 
+            from Music
+            where id in (?)
+            `,
+            [musicIds]
+        );
+
+        const musicInfo: MappedTrack[] = musicInfoRaw.map(music => ({
+            id: music.id,
+            name: music.name,
+            albumArt: music.album_art_url,
+            musicUrl: music.music_url,
+            artist: music.artist,
+            artistUrl: music.artist_url,
+            preview_url: music.preview_url
+        }));
+
+        return { posts, musicInfo };
+    } catch (err) {
+        console.error("Error Fetching latest posts", err);
+        throw new Error("Failed to fetch latest posts");
+    }
+};
