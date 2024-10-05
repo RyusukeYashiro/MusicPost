77d3b01e946d50318ebcf5836fda19bdb1a9d15e
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-10-06 04:45:13 +0900

    server actionsのプロップス , homdコンポーネント

--- musicpost/src/app/homePost/page.tsx
+++ musicpost/src/app/homePost/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Sidebar from "../../components/SideBar/Sidebar";
+import HomePost from "../../components/homePost/homePost";
+import { getLatestPost } from "../action";
 
-const SidebarPage = () => {
+const home = async () => {
+  //初期データを取得する
+  const initialPosts = await getLatestPost();
+
   return (
     <div className="App">
       <Sidebar />
+      <HomePost initialPosts={initialPosts} />
     </div>
   );
 };
 
-export default SidebarPage;
+export default home;
---
+++ musicpost/src/components/homePost/homePost.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import React, { useEffect, useRef, useState } from "react";
+import { getLatestPost } from "@/app/action";
+import { MappedTrack } from "@/types/mappedTrack";
+import { handlePlayPause } from "@/utils/Musichandle";
+import PauseCircleOutlineIcon from "@mui/icons-material/PauseCircleOutline";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import Image from "next/image";
+import noimage from "../../../public/images/3.png";
+import { usePathname } from "next/navigation";
+
+export interface Post {
+    user_id: number;
+    user_name: string;
+    music_id: string;
+    content: string;
+}
+
+interface PostData {
+    posts: Post[];
+    musicInfo: MappedTrack[];
+}
+
+interface HomePostProps {
+    initialPosts: PostData;
+}
+
+const HomePost: React.FC<HomePostProps> = ({ initialPosts }) => {
+    const [allPost, setPosts] = useState<PostData>(initialPosts);
+    const [playingTrackId, setPlayingTrackId] = useState<string | null>(null);
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+    const pathname = usePathname();
+
+    const refreshPost = async () => {
+        const latestPosts = await getLatestPost();
+        setPosts(latestPosts);
+        console.log('latestPost', latestPosts);
+    }
+
+
+    useEffect(() => {
+        if (pathname === '/homePost') {
+            refreshPost();
+        }
+    }, [pathname]);
+    return (
+        <div>
+            <h2>homeアイコンで最新のツイートを!</h2>
+            <ul className="post-list">
+                {allPost.posts.map((post) => {
+                    const music = allPost.musicInfo.find((m) => m.id === post.music_id);
+                    if (!music) return null;
+                    return (
+                        <li key={`${post.user_id}-${music.id}`}>
+                            <div className="post-item">
+                                <div>{post.user_name}</div>
+                                <div className="post-music">
+                                    <div className="music-select">
+                                        <Image
+                                            alt={`Album art for ${music.name}`}
+                                            src={music.albumArt || noimage}
+                                            width={80}
+                                            height={80}
+                                            className="rounded-md"
+                                        />
+                                    </div>
+                                    <h3 className="font-bold">
+                                        <a
+                                            href={music.musicUrl}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="text-[#639bb7] hover:text-[#1abc54] transition-colors duration-300"
+                                        >
+                                            {music.name}
+                                        </a>
+                                    </h3>
+                                    <p className="text-gray-600">
+                                        <a
+                                            href={music.artistUrl}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="text-[#639bb7] hover:text-[#1abc54] transition-colors duration-300"
+                                        >
+                                            {music.artist}
+                                        </a>
+                                    </p>
+                                    {music.preview_url && (
+                                        <button
+                                            className="music-play-pause"
+                                            onClick={() => handlePlayPause(music, playingTrackId, setPlayingTrackId, audioRef)}
+                                        >
+                                            {playingTrackId === music.id ? (
+                                                <PauseCircleOutlineIcon className="mx-4" sx={{ fontSize: 40 }} />
+                                            ) : (
+                                                <PlayCircleOutlineIcon className="mx-4" sx={{ fontSize: 40 }} />
+                                            )}
+                                        </button>
+                                    )}
+                                </div>
+                                <div className="post-content">
+                                    <div className="contents">{post.content}</div>
+                                </div>
+                            </div>
+                        </li>
+                    )
+                }
+                )}
+            </ul>
+            <audio ref={audioRef} />
+        </div>
+    );
+};
+
+export default HomePost;
