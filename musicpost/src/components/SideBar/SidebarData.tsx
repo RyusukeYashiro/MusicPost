aec1a52becc1bff0cc1059e2793293f4c5299d7b
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-10-08 18:58:57 +0900

    next.jsのダイナミックルーティングの設定の仕方

--- musicpost/src/components/SideBar/SidebarData.tsx
+++ musicpost/src/components/SideBar/SidebarData.tsx
@@ -3,6 +3,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import BorderColorSharpIcon from "@mui/icons-material/BorderColorSharp";
 import LogoutSharpIcon from "@mui/icons-material/LogoutSharp";
+import { JwtPayload } from "jsonwebtoken";
 
 interface SidebarItem {
   title: string;
@@ -10,27 +11,38 @@ interface SidebarItem {
   link: string;
 }
 
-const SidebarData: () => SidebarItem[] = () => [
-  {
-    title: "ホーム",
-    icon: <HomeIcon />,
-    link: "/homePost",
-  },
-  {
-    title: "ユーザー",
-    icon: <PersonIcon />,
-    link: "/userSet",
-  },
-  {
-    title: "投稿",
-    icon: <BorderColorSharpIcon />,
-    link: "/createPost",
-  },
-  {
-    title: "ログアウト",
-    icon: <LogoutSharpIcon />,
-    link: "/logOut",
-  },
-];
+type UserInfo = string | JwtPayload | null | undefined;
+
+//ダイナミックルーティングを使用
+const SidebarData = (name: UserInfo): SidebarItem[] => {
+  const userLink = (() => {
+    if (!name) return '/login';
+    const username = typeof name === 'string' ? name : name.username;
+    return username ? `/userSet/${encodeURIComponent(username)}` : '/Login';
+  })();
+
+  return [
+    {
+      title: "ホーム",
+      icon: <HomeIcon />,
+      link: "/homePost",
+    },
+    {
+      title: "ユーザー",
+      icon: <PersonIcon />,
+      link: userLink,
+    },
+    {
+      title: "投稿",
+      icon: <BorderColorSharpIcon />,
+      link: "/createPost",
+    },
+    {
+      title: "ログアウト",
+      icon: <LogoutSharpIcon />,
+      link: "/logOut",
+    },
+  ];
+};
 
 export default SidebarData;
